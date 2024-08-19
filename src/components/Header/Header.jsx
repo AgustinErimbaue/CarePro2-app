040340c4943df35e200912d566e90a8c7a2a56bb
Agustin Bernabe Erimbaue Lamontanaro <agustibernabe@gmail.com>
2024-08-19 18:32:54 +0200

    Se crea prouecto y se agrega la carpeta header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Header.css" 
+
+const Header = () => {
+  return (
+    <div>
+      <ul>
+        <li>Inicio</li>
+        <li>Como funciona?</li>
+        <li>Contacto</li>
+        <li>Registrarse</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Header;
