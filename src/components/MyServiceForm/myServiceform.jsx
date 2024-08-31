847298681bbedf3657d438bdf7aae0bde99d867a
Agustin Bernabe Erimbaue Lamontanaro <agustibernabe@gmail.com>
2024-08-31 13:12:49 +0200

    se crea formulario con su correspondiente input

--- src/components/MyServiceForm/myServiceform.jsx
+++ src/components/MyServiceForm/myServiceform.jsx
@@ -1,14 +1,46 @@
 import React, { useState } from "react";
 
 const myServiceform = () => {
-  const [data, setData] = useState({
-    title: "",
-    description: "",
-    price: "",
-    provider: "",
-    category: "",
-  });
-  return <div>myServiceform</div>;
+  const initialState = { title: "", description: "", price: 0, category: "" };
+
+  const [data, setData] = useState(initialState);
+  const handleInputChange = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <form>
+      <input
+        type="text"
+        placeholder="Ingrese titulo del servicio"
+        value={data.title}
+        name="title"
+        onChange={handleInputChange}
+      />
+      <input
+        type="text"
+        placeholder="Ingrese descripcion del servicio"
+        value={data.description}
+        name="description"
+        onChange={handleInputChange}
+      />
+      <input
+        type="number"
+        placeholder="Ingrese precio"
+        value={data.price}
+        name="price"
+        onChange={handleInputChange}
+      />
+      <input
+        type="text"
+        placeholder="Ingrese categoria de servicio"
+        value={data.category}
+        name="category"
+        onChange={handleInputChange}
+      />
+      <button>Agregar servicio</button>
+    </form>
+  );
 };
 
 export default myServiceform;
