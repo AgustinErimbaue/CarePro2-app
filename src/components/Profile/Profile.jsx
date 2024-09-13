c0277afcc66bb5c50ef2d1ddced9aa1ff748dc9c
Agustin Bernabe Erimbaue Lamontanaro <agustibernabe@gmail.com>
2024-09-13 11:17:09 +0200

    se agrega div con className contracted-service

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,18 +4,22 @@ import { useDispatch, useSelector } from "react-redux";
 import { getLoggedUser } from "../../features/auth/authSlice";
 
 const Profile = () => {
+  const { user } = useSelector((state) => state.auth);
+
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(getLoggedUser());
   }, [dispatch]);
-  const { user } = useSelector((state) => state.auth);
   return (
     <div className="profile-container">
       <h1>Perfil</h1>
       <div className="info-container">
         <div className="img-profile">
-          <img src={user?.profilePicture || "default-profile.png"} alt="Profile" />
+          <img
+            src={user?.profilePicture || "default-profile.png"}
+            alt="Profile"
+          />
         </div>
         <div className="info">
           <div className="name-user">
@@ -23,15 +27,30 @@ const Profile = () => {
           </div>
           <div className="rate-user">
             {Array.from({ length: 5 }, (_, index) => (
-              <span key={index} className={index < (user?.rating || 0) ? 'filled-star' : 'empty-star'}>&#9733;</span>
+              <span
+                key={index}
+                className={
+                  index < (user?.rating || 0) ? "filled-star" : "empty-star"
+                }
+              >
+                &#9733;
+              </span>
             ))}
           </div>
           <div className="extra-info">
-            <p><strong>Email:</strong> {user?.email || "Not provided"}</p>
-            <p><strong>Phone Number:</strong> {user?.phoneNumber || "Not provided"}</p>
-            <p><strong>Address:</strong> {user?.address || "Not provided"}</p>
+            <p>
+              <strong>Email:</strong> {user?.email || "Not provided"}
+            </p>
+            <p>
+              <strong>Phone Number:</strong>{" "}
+              {user?.phoneNumber || "Not provided"}
+            </p>
+            <p>
+              <strong>Address:</strong> {user?.address || "Not provided"}
+            </p>
           </div>
         </div>
+        <div className="contracted-service"></div>
       </div>
     </div>
   );
