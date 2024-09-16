ac49ecee4877457d1c8f227a47a9cebd87964ebe
Agustin Bernabe Erimbaue Lamontanaro <agustibernabe@gmail.com>
2024-09-16 19:59:15 +0200

    se agrega funcion para contratar servicio en contractService

--- src/features/contract/contractService.js
+++ src/features/contract/contractService.js
@@ -1,2 +1,16 @@
 import axios from "axios";
-const API_URL= "http://localhost:8080"
+const API_URL = "http://localhost:8080";
+
+const hireService = async (service) => {
+  const token = localStorage.getItem("token");
+  const res = await axios.post(`${API_URL}/contracts/hire`, service, {
+    headers: { Authorization: token },
+  });
+  return res.data;
+};
+
+const contractService = {
+  hireService,
+};
+
+export default contractService;
